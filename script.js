e8939b973072df84a75cec0e8f000f07caa5c239
GrootStudent <borodichalexander97@gmail.com>
2020-03-16 18:48:18 +0300

    feat: implement nav-menu

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 // header menu
-let navMenu = document.querySelector('#nav-menu');
+let navMenu = document.querySelector('.nav-menu__list');
+let logo = document.querySelector('#logo');
+let toggleMenu = document.querySelector('#toggle-menu');
 
 navMenu.addEventListener("click", (event) => {
-	let prevLink = document.querySelector('#nav-menu .nav-menu__link--active');
+	let prevLink = document.querySelector('.nav-menu__list .nav-menu__link--active');
 	let activeLink = event.target;
 
 	prevLink.classList.remove('nav-menu__link--active');
 	activeLink.classList.add('nav-menu__link--active');
+
+	if (logo.classList.contains('is-open')) {
+		toggleMenu.click();
+	}
 }, false);
+
+
+toggleMenu.addEventListener('click', (event) => {
+	event.preventDefault();
+
+	let menu = document.querySelector('.nav-menu');
+
+	menu.classList.toggle('is-open');
+	logo.classList.toggle('is-open');
+	document.querySelector('body').classList.toggle('is-open-menu');
+
+})
