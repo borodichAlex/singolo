623aa822dd9c29e892da9ee44addcbd76521783a
GrootStudent <borodichalexander97@gmail.com>
2020-03-16 23:22:41 +0300

    feat: add script for slider

--- script.js
+++ script.js
@@ -26,3 +26,48 @@ toggleMenu.addEventListener('click', (event) => {
 	document.querySelector('body').classList.toggle('is-open-menu');
 
 })
+
+// slider
+let slider = document.querySelector('.slider');
+let slides = [...document.querySelectorAll('.slider__slide')];
+let arrowRight = document.querySelector('.slider__arrow--right');
+let arrowLeft = document.querySelector('.slider__arrow--left');
+
+arrowRight.addEventListener("click", (event) => {
+	slider.classList.toggle('slider--bg_primary');
+	if (slides[slides.length - 1].classList.contains('slider__slide--current')) {
+		slides[slides.length - 1].classList.remove('slider__slide--current');
+		slides[0].classList.add('slider__slide--current');
+	} else if (slides[0].classList.contains('slider__slide--current')) {
+		slides[0].classList.remove('slider__slide--current');
+		slides[1].classList.add('slider__slide--current');
+	}
+
+}, false);
+
+arrowLeft.addEventListener("click", (event) => {
+	slider.classList.toggle('slider--bg_primary');
+	if (slides[slides.length - 1].classList.contains('slider__slide--current')) {
+		slides[slides.length - 1].classList.remove('slider__slide--current');
+		slides[0].classList.add('slider__slide--current');
+	} else if (slides[0].classList.contains('slider__slide--current')) {
+		slides[0].classList.remove('slider__slide--current');
+		slides[1].classList.add('slider__slide--current');
+	}
+
+}, false);
+
+
+//telephone
+
+let button = [...document.querySelectorAll('.telephone .telephone__button')];
+
+button.forEach((btn) => {
+	btn.addEventListener('click', event => {
+		let telephoneScreen = btn.previousElementSibling;
+		telephoneScreen.classList.toggle('telephone__screen--hidden');
+	})
+})
+
+
+
